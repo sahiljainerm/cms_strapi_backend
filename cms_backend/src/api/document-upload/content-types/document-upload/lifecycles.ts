6b33fb53a390704b5dd0336dc8bb53be735962e8
sahiljainerm <sahil.jain@erm.com>
2025-07-11 16:45:17 +0530

    Update lifecycles.ts

--- cms_backend/src/api/document-upload/content-types/document-upload/lifecycles.ts
+++ cms_backend/src/api/document-upload/content-types/document-upload/lifecycles.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import uploadDoc from '../../../document-upload/controllers/document-upload';
-
+import axios from 'axios';
 export default {
   async afterCreate(event) {
     const entry = await strapi.entityService.findOne('api::document-upload.document-upload', event.result.id, {
@@ -22,10 +22,28 @@ export default {
     }
 
     const filePath = file.url//path.resolve(relativeUrl.replace(/^\/+/, ''));
-    if (!fs.existsSync(filePath)) {
-      strapi.log.error(`File not found: ${filePath}`);
-      return;
+    
+    
+
+async function checkFileExists(filePath: string): Promise<boolean> {
+  if (/^https?:\/\//i.test(filePath)) {
+    // For remote URL, check by making HEAD request
+    try {
+      await axios.head(filePath);
+      return true;
+    } catch {
+      return false;
     }
+  } else {
+    // Local path
+    return fs.existsSync(filePath);
+  }
+}
+    const file_exists = await checkFileExists(filePath);
+if (!file_exists) {
+  strapi.log.error(`File not found: ${filePath}`);
+  return;
+}
 
     await uploadDoc.processCsvFileFromPath(filePath);
     strapi.log.info(`✅ Processed CSV: ${filePath}`);
