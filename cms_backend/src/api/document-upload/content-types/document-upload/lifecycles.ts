2567fbd45ac78ce16ee123a303c14f70dccb7692
sahiljainerm <sahil.jain@erm.com>
2025-07-11 16:15:28 +0530

    Update lifecycles.ts

--- cms_backend/src/api/document-upload/content-types/document-upload/lifecycles.ts
+++ cms_backend/src/api/document-upload/content-types/document-upload/lifecycles.ts
@@ -21,7 +21,7 @@ export default {
       return;
     }
 
-    const filePath = path.resolve(relativeUrl.replace(/^\/+/, ''));
+    const filePath = file.url//path.resolve(relativeUrl.replace(/^\/+/, ''));
     if (!fs.existsSync(filePath)) {
       strapi.log.error(`File not found: ${filePath}`);
       return;
